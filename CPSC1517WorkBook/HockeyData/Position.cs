8ce07febeaf61289d71f6ac23522e3b6e87eee3d
Wizzard2001 <rpossberg1@nait.ca>
2023-09-07 21:26:14 -0600

    Added HockeyPlayer.cs Position.cs Shot.cs

---
+++ CPSC1517WorkBook/HockeyData/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace Hockey.Data
+{
+    /// <summary>
+    ///  Represents the available hockey positions.
+    /// </summary>
+    public enum Position
+    {
+        LeftWing = 1,
+        Center,
+        RightWing,
+        Defense,
+        Goalie
+    }
+
+}
---
+++ CPSC1517WorkBook/HockeyData/Shot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HockeyData
+{
+    internal class Shot
+    {
+        // data fields
+        private string _name;
+
+        // properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // constructors
+        public Shot(string name)
+        {
+            _name = name;
+        }
+    }
+}
