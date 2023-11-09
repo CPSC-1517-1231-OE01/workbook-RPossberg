73da8fe7b2e5bd4d9e95ac18bf305a5c5302682f
Wizzard2001 <rpossberg1@nait.ca>
2023-11-09 13:37:51 -0700

    Added CategoryProducts and services

---
+++ CPSC1517WorkBook/WestWindSystem/BLL/CategoryServices.cs
@@ -0,0 +1,24 @@
+﻿using WestWindSystem.DAL;
+using WestWindSystem.ENTITIES;
+
+namespace WestWindSystem.BLL
+{
+    public class CategoryServices
+    {
+        private readonly WestWindContext _context;
+
+        internal CategoryServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns all categories in the system
+        /// </summary>
+        /// <returns>A list of categories, if any were found</returns>
+        public List<Category>? GetCategories()
+        {
+            return _context.Categories.ToList<Category>();
+        }
+    }
+}
